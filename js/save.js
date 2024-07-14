7571a70034f581e07a8e1c5601aa516048fba023
jonas-1997 <jvhencke@googlemail.com>
2024-07-14 12:44:37 +0200

    Add more functionality to save fuction

--- js/save.js
+++ js/save.js
@@ -1,3 +1,7 @@
+import setBackground from "./background.js";
+import setPlaylist from "./playlist.js";
+import { changeSelectedTime } from "./timer.js";
+
 const timerPomodoro      = document.querySelector('#timer-pomodoro');
 const timerShortBreak   = document.querySelector('#timer-short-break');
 const timerLongBreak    = document.querySelector('#timer-long-break');
@@ -7,11 +11,15 @@ const backgroundSelect  = document.querySelector('#background-select');
 const playlistSelect    = document.querySelector('#playlist-select');
 
 export default function saveChanges() {
-    localStorage.setItem('pomdoroLength', timerPomodoro.value);
+    localStorage.setItem('pomodoroLength', timerPomodoro.value);
     localStorage.setItem('shortBreakLength', timerShortBreak.value);
     localStorage.setItem('longBreakLength', timerLongBreak.value);
     localStorage.setItem('alarm', alarmSelect.value);
     localStorage.setItem('volume', volumeRange.value);
     localStorage.setItem('background', backgroundSelect.value);
     localStorage.setItem('playlist', playlistSelect.value);
+
+    setBackground(backgroundSelect.value);
+    setPlaylist(playlistSelect.value);
+    changeSelectedTime(timerPomodoro.value, timerShortBreak.value, timerLongBreak.value);
 }
