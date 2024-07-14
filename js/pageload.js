6f27d2e739bcbfa0152aeda6a142d260ced9f20f
jonas-1997 <jvhencke@googlemail.com>
2024-07-14 13:06:54 +0200

    Implement pageload functionality

---
+++ js/pageload.js
@@ -0,0 +1,20 @@
+import { changeSelectedTime } from "./timer";
+import setBackground from "./background";
+import setPlaylist from "./playlist";
+
+
+export default function pageload() {
+    changeSelectedTime(localStorage.getItem('pomodoroLength') ? localStorage.getItem('pomodoroLength') : 25);
+    setBackground(localStorage.getItem('background'));
+    setPlaylist(localStorage.getItem('playlist'));
+
+    //enter modal input values from localStorage
+    document.getElementById('timer-pomodoro').value = localStorage.getItem('pomodoroLength');
+    document.getElementById('timer-short-break').value = localStorage.getItem('shortBreakLength');
+    document.getElementById('timer-long-break').value = localStorage.getItem('longBreakLength');
+
+    //select options from localStorage
+    document.getElementById('alarm-select').querySelector(`option[value='${localStorage.getItem('alarm')}']`).setAttribute('selected', true);
+    document.getElementById('background-select').querySelector(`option[value='${localStorage.getItem('background')}']`).setAttribute('selected', true);
+    document.getElementById('playlist-select').querySelector(`option[value='${localStorage.getItem('playlist')}']`).setAttribute('selected', true);
+}
