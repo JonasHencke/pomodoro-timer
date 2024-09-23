949ca1de2c8f821c09dcbd0cdd5b1c61bc97f259
jonas-1997 <jvhencke@googlemail.com>
2024-09-23 21:52:47 +0200

    Add new background option

--- js/background.js
+++ js/background.js
@@ -1,16 +1,19 @@
 export default function setBackground(name) {
     switch (name) {
         case 'gas_station':
-            document.body.style.background = 'url(https://images.pexels.com/photos/5411700/pexels-photo-5411700.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2) 31% center / cover no-repeat';
+            document.body.style.background = 'url(https://images.pexels.com/photos/5411700/pexels-photo-5411700.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2) 50% center / cover no-repeat';
             break;
         case 'starry_night':
             document.body.style.background = 'url(https://images.pexels.com/photos/1567069/pexels-photo-1567069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2) 25% center / cover no-repeat fixed ';
             break;
         case 'virtual_reality':
-            document.body.style.background = 'url("https://images.pexels.com/photos/8721342/pexels-photo-8721342.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 25% center / cover no-repeat fixed';
+            document.body.style.background = 'url("https://images.pexels.com/photos/8721342/pexels-photo-8721342.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 58% center / cover no-repeat fixed';
             break;
         case 'tokyo_tower':
             document.body.style.background = 'url(https://images.pexels.com/photos/4410426/pexels-photo-4410426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2) 20%';
+            break;
+        case 'mountains':
+            document.body.style.background = 'url(https://images.pexels.com/photos/1743381/pexels-photo-1743381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2) 50% center / cover no-repeat';
             break;
         }
     }
