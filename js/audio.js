d0aa31a0cd4171c5967f90a8fca8de58ff59dd3d
jonas-1997 <jvhencke@googlemail.com>
2024-09-24 21:22:45 +0200

    Implement new alarm options

--- js/audio.js
+++ js/audio.js
@@ -13,6 +13,20 @@ function returnRingtonePath(value) {
             return 'ringtones/happy_jingle.mp3';
         case 'glass_bubbles':
             return 'ringtones/glass_bubbles.mp3';
+        case 'alarm_clock':
+            return 'ringtones/alarm_clock.mp3';
+        case 'bell':
+            return 'ringtones/Bell.mp3';
+        case 'chime':
+            return 'ringtones/chime.mp3';
+        case 'choir':
+            return 'ringtones/choir.mp3';
+        case 'dreamy':
+            return 'ringtones/dreamy.mp3';
+        case 'echoes':
+            return 'ringtones/echoes.mp3';
+        case 'level_up':
+            return 'ringtones/level_up.mp3';
         default:
             return 'ringtones/standard.mp3';
         }
