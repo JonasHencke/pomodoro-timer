9b9a1e1062cbc26385bc7893cdd0612767243143
jonas-1997 <jvhencke@googlemail.com>
2024-09-25 21:54:33 +0200

    Add more background options

--- js/background.js
+++ js/background.js
@@ -15,5 +15,38 @@ export default function setBackground(name) {
         case 'mountains':
             document.body.style.background = 'url(https://images.pexels.com/photos/1743381/pexels-photo-1743381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2) 50% center / cover no-repeat';
             break;
+        case 'corals':
+            document.body.style.background = 'url("https://images.pexels.com/photos/920163/pexels-photo-920163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 25% center / cover no-repeat fixed';
+            break;
+        case 'jellyfish':
+            document.body.style.background = 'url("https://images.pexels.com/photos/2832767/pexels-photo-2832767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")';
+            break;
+        case 'autumn':
+            document.body.style.background = 'url("https://images.pexels.com/photos/1102912/pexels-photo-1102912.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 25% center / cover no-repeat fixed';
+            break;
+        case 'foggy_mountains':
+            document.body.style.background = 'url("https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 25% center / cover no-repeat fixed';
+            break;
+        case 'smooth_clouds':
+            document.body.style.background = 'url("https://images.pexels.com/photos/2114014/pexels-photo-2114014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")';
+            break;
+        case 'cumulus_clouds':
+            document.body.style.background = 'url("https://images.pexels.com/photos/1605148/pexels-photo-1605148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 15% center / cover no-repeat fixed';
+            break;
+        case 'architecure':
+            document.body.style.background = 'url("https://images.pexels.com/photos/28570477/pexels-photo-28570477/free-photo-of-reflektierende-stadtlandschaft-bei-sonnenuntergang-mit-blick-auf-den-fluss.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 25% center';
+            break;
+        case 'waves':
+            document.body.style.background = 'url("https://images.pexels.com/photos/15272404/pexels-photo-15272404/free-photo-of-ufer-aufsicht-draufsicht-spritzen.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 25% center';
+            break;
+        case 'lotus_flower':
+            document.body.style.background = 'url("https://images.pexels.com/photos/28579274/pexels-photo-28579274/free-photo-of-leuchtend-rosa-seerose-in-ruhigen-teich.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 25% center';
+            break;
+        case 'ancient':
+            document.body.style.background = 'url("https://images.pexels.com/photos/2121549/pexels-photo-2121549.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2") 50% center';
+            break;
+        default:
+            document.body.style.background = 'url(https://images.pexels.com/photos/1567069/pexels-photo-1567069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2) 25% center / cover no-repeat fixed ';
+            break;
         }
     }
