35edcb605b7cef6ee605164b7bbf2e9b4b5a5b99
jonas-1997 <jvhencke@googlemail.com>
2024-10-12 22:42:55 +0200

    refactor timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,23 +1,25 @@
 import { playAudio, pauseAudio } from "./audio";
 import { clearInterval, clearTimeout, setInterval, setTimeout } from 'worker-timers';
-export let timer;
-export let minutes = 25;
-export let seconds = 0;
-export let isPaused = true;
+
+let isPaused = true;
+let timerInterval;
 let audioTimeout;
+
+//durations
+let minutes = 25;
+let seconds = 0;
 let timeSelected = 25;
-let pomodoroLength = parseInt(localStorage.getItem('pomodoroLength')) ? localStorage.getItem('pomodoroLength') : 25;
-let shortBreakLength = parseInt(localStorage.getItem('shortBreakLength')) ? localStorage.getItem('shortBreakLength') : 5;
-let longBreakLength = parseInt(localStorage.getItem('longBreakLength')) ? localStorage.getItem('longBreakLength') : 15;
+
+//DOM elements
 let playBtn = document.querySelector("#playBtn");
 let pauseBtn = document.querySelector("#pauseBtn");
 let Timer = document.getElementById('timer');
 
-
+//functions
 export function startTimer() {
     if (isPaused) {
         isPaused = false;
-        timer = setInterval(updateTimer, 1000);
+        timerInterval = setInterval(updateTimer, 1000);
         playBtn.classList.add('d-none');
         pauseBtn.classList.remove('d-none');
         playAudio();
@@ -29,7 +31,7 @@ export function startTimer() {
 
 export function pauseTimer() {
     isPaused = true;
-    clearInterval(timer);
+    clearInterval(timerInterval);
     pauseBtn.classList.add('d-none');
     playBtn.classList.remove('d-none');
     clearTimeout(audioTimeout);
@@ -37,7 +39,7 @@ export function pauseTimer() {
 
 export function resetTimer() {
     isPaused = true;
-    clearInterval(timer);
+    clearInterval(timerInterval);
     clearTimeout(audioTimeout);
     minutes = timeSelected;
     seconds = 0;
@@ -46,7 +48,7 @@ export function resetTimer() {
     playBtn.classList.remove('d-none');
 }
 
-export function updateTimer() {
+function updateTimer() {
     if (minutes === 0 && seconds === 0) {
         resetTimer();
     } else if (seconds === 0) {
@@ -59,7 +61,7 @@ export function updateTimer() {
     updateDisplay();
 }
 
-export function updateDisplay() {
+function updateDisplay() {
     const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
     const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
     Timer.innerText = `${formattedMinutes}:${formattedSeconds}`;
@@ -73,12 +75,4 @@ export function changeSelectedTime(length) {
     minutes = length;
 
     updateDisplay();
-}
-
-
-
-export function ChangeTimerLengths(pomodoro, short, long) {
-    pomodoroLength = pomodoro ? pomodoro : pomodoroLength;
-    shortBreakLength = short ? short : shortBreakLength;
-    longBreakLength = long ? long : longBreakLength;
 }
